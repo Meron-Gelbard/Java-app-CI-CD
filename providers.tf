42e23f94350dc42ee4ea06a8ab8cc67fffa21600
Meron Gelbard <merongelbard@gmail.com>
2024-01-05 13:19:37 +0200

    Added terraform test6

--- providers.tf
+++ providers.tf
@@ -5,15 +5,17 @@ terraform {
       version = "~> 4.2.0"
     }
   }
-  backend "s3" {
-    bucket         = "terafform-backend"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-    shared_credentials_file = "/home/ubuntu/.aws/credentials"
-    profile                 = "default"
-  }
+
+  #backend "s3" {
+  #  bucket         = "terafform-backend"
+  #  key            = "global/s3/terraform.tfstate"
+  #  region         = "us-east-1"
+  #  dynamodb_table = "terraform-lock"
+  #  encrypt        = true
+  #  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  #  profile                 = "default"
+  #}
+  
 }
 provider "aws" {
   region     = "us-east-1"
