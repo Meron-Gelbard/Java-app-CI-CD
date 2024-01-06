ab4a069b8bf955ca355d5a7b2d5c0a76a575728d
Meron Gelbard <merongelbard@gmail.com>
2024-01-06 23:12:46 +0200

    version script test1

---
+++ app_version_inc.py
@@ -0,0 +1,14 @@
+
+with open('java_app_version.txt', "r") as v_file:
+    text = v_file.read()
+    current_version = text.split(" ")[3]
+    current_version = current_version.split(".")
+
+current_version[2] = str(int(current_version[2]) + 1)
+new_version = ".".join(current_version[:])
+
+with open('java_app_version.txt', "w") as v_file:
+    new_text = (" ".join(text.split(" ")[:3])) + f" {new_version}"
+    v_file.write(new_text)
+
+print(new_version)
