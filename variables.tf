7b6369d4e56d9a4603931653904fd972fec757da
Meron Gelbard <merongelbard@gmail.com>
2024-01-08 15:30:28 +0200

    FINAL test-7

--- variables.tf
+++ variables.tf
@@ -17,3 +17,20 @@ variable "vpc_id" {
 variable "subnet_id" {
   description = "subnet id"
 }
+
+variable "aws_region" {
+  description = "aws region"
+}
+
+provider "external" {
+  version = "1.2.0"
+}
+
+data "external" "ip_script_output" {
+  program = ["python3", "${path.module}/ec2_connect_ip_script.py", "${var.aws_region}"]
+}
+
+variable "ec2_connect_ip" {
+  type    = string
+  default = data.external.ip_script_output.result
+}
