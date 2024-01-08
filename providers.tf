77c439180f932417cc1a749e01696500c7b9deae
Meron Gelbard <merongelbard@gmail.com>
2024-01-08 15:34:55 +0200

    FINAL test-7

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket         = "terafform-backend"
     key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "${var.aws_region}"
     dynamodb_table = "terraform-lock"
     encrypt        = true
     shared_credentials_file = "/home/ubuntu/.aws/credentials"
